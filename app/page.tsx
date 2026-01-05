de7ed73a3546eb52be3d6b1f6b6d0b3bb8f73878
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-05 18:02:39 +0530

    feat: home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,42 @@
 import React from "react";
+import Image from "next/image";
+import DataTable from "@/components/DataTable";
+import Link from "next/link";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {header: "Name",
+    cellClassName:"name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+
+      return (
+          <Link href={`/coins/${item.id}`}>
+              <Image src={item.large} alt={item.name} width={36} height={36}/>
+          </Link>
+      )
+    }}
+]
 
 const Page = () => {
-  return <div></div>;
+  return <main className="main-container">
+    <section className="home-grid">
+      <div id="coin-overview">
+        <div className="header pt-2">
+          <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56}/>
+          <div className="info">
+            <p>Bitcoin / BTC</p>
+            <h1>$89,113.00</h1>
+          </div>
+        </div>
+      </div>
+
+      <p>Trending Coins</p>
+     <DataTable data={[]} columns={[]}/>
+    </section>
+
+    <section className="w-full mt-7 space-y-4">
+      <p>Categories</p>
+    </section>
+  </main>;
 };
 export default Page;
