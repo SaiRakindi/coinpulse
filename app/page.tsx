9a3525f0afca375ad12f23291044c568f93546ba
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-10 22:38:13 +0530

    feat: code formatting using prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import Image from "next/image";
 import DataTable from "@/components/DataTable";
 import Link from "next/link";
-import {cn} from "@/lib/utils";
-import {TrendingDown, TrendingUp} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
 
 const columns: DataTableColumn<TrendingCoin>[] = [
   {
     header: "Name",
-    cellClassName:"name-cell",
+    cellClassName: "name-cell",
     cell: (coin) => {
       const item = coin.item;
 
       return (
-          <Link href={`/coins/${item.id}`}>
-              <Image src={item.large} alt={item.name} width={36} height={36}/>
-              <p>{item.name}</p>
-          </Link>
-      )
-    }
+        <Link href={`/coins/${item.id}`}>
+          <Image src={item.large} alt={item.name} width={36} height={36} />
+          <p>{item.name}</p>
+        </Link>
+      );
+    },
   },
   {
     header: "24h Change",
@@ -28,38 +28,52 @@ const columns: DataTableColumn<TrendingCoin>[] = [
       const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
 
       return (
-          <div className={cn("price-change", isTrendingUp ? "text-green-500": "text-red-500")}>
-            <p>
-              {isTrendingUp ? <TrendingUp width={16} height={16}/>
-                  : <TrendingDown width={16} height={16}/>
-              }
-            </p>
-          </div>
-      )
-    }
-  }
-]
+        <div
+          className={cn(
+            "price-change",
+            isTrendingUp ? "text-green-500" : "text-red-500"
+          )}
+        >
+          <p>
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) : (
+              <TrendingDown width={16} height={16} />
+            )}
+          </p>
+        </div>
+      );
+    },
+  },
+];
 
 const Page = () => {
-  return <main className="main-container">
-    <section className="home-grid">
-      <div id="coin-overview">
-        <div className="header pt-2">
-          <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56}/>
-          <div className="info">
-            <p>Bitcoin / BTC</p>
-            <h1>$89,113.00</h1>
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              alt="Bitcoin"
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1>$89,113.00</h1>
+            </div>
           </div>
         </div>
-      </div>
 
-      <p>Trending Coins</p>
-     <DataTable data={[]} columns={[]} rowKey={}/>
-    </section>
+        <p>Trending Coins</p>
+        <DataTable data={[]} columns={[]} rowKey={} />
+      </section>
 
-    <section className="w-full mt-7 space-y-4">
-      <p>Categories</p>
-    </section>
-  </main>;
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 export default Page;
