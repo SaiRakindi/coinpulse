074408262e4d62692ef2be6014d57965516212ba
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-18 20:21:17 +0530

    feat: coingecko api integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import DataTable from "@/components/DataTable";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { cn, formatCurrency } from "@/lib/utils";
 import { TrendingDown, TrendingUp } from "lucide-react";
+import { fetcher } from "@/lib/coingecko.actions";
 
 const columns: DataTableColumn<TrendingCoin>[] = [
   {
@@ -51,21 +52,27 @@ const columns: DataTableColumn<TrendingCoin>[] = [
   },
 ];
 
-const Page = () => {
+const Page = async () => {
+  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
+    dex_pair_format: "symbol",
+  });
+
   return (
     <main className="main-container">
       <section className="home-grid">
         <div id="coin-overview">
           <div className="header pt-2">
             <Image
-              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              src={coin.image.large}
               alt="Bitcoin"
               width={56}
               height={56}
             />
             <div className="info">
-              <p>Bitcoin / BTC</p>
-              <h1>$89,113.00</h1>
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
             </div>
           </div>
         </div>
@@ -73,7 +80,9 @@ const Page = () => {
         <p>Trending Coins</p>
         <DataTable
           data={[]}
-          columns={[{ header: "Title" }, { header: "Price" }]}
+          columns={columns}
+          rowKey={(coin) => coin.item.id}
+          tableClassName="trending-coins-table"
         />
       </section>
 
