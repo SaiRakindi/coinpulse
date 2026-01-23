92cde23083afef4bf30e2336b6c7d7c7ff917d75
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-23 13:33:06 +0530

    feat: data fetching using fallback

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -2,11 +2,19 @@ import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+import { CoinOverviewFallback } from "./fallback";
 
 const CoinOverview = async () => {
-  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-    dex_pair_format: "symbol",
-  });
+  let coin;
+
+  try {
+    coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
+      dex_pair_format: "symbol",
+    });
+  } catch (error) {
+    console.error("Error fetching coin details:", error);
+    return <CoinOverviewFallback />;
+  }
 
   return (
     <div id="coin-overview">
--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -4,13 +4,21 @@ import Image from "next/image";
 import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import DataTable from "@/components/DataTable";
+import { TrendingCoinsFallback } from "./fallback";
 
 const TrendingCoins = async () => {
-  const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
-    "/search/trending",
-    undefined,
-    300
-  );
+  let trendingCoins;
+
+  try {
+    trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
+      "/search/trending",
+      undefined,
+      300
+    );
+  } catch (error) {
+    console.error("Error fetching trending coins:", error);
+    return <TrendingCoinsFallback />;
+  }
 
   const columns: DataTableColumn<TrendingCoin>[] = [
     {
