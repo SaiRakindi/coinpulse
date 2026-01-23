03b91085cd05f762b569ec4868a7f705d0e8a5a6
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-23 13:28:12 +0530

    feat: home trending coins and overview components added

--- app/page.tsx
+++ app/page.tsx
@@ -4,86 +4,31 @@ import Link from "next/link";
 import { cn, formatCurrency } from "@/lib/utils";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import { fetcher } from "@/lib/coingecko.actions";
-
-const columns: DataTableColumn<TrendingCoin>[] = [
-  {
-    header: "Name",
-    cellClassName: "name-cell",
-    cell: (coin) => {
-      const item = coin.item;
-
-      return (
-        <Link href={`/coins/${item.id}`}>
-          <Image src={item.large} alt={item.name} width={36} height={36} />
-          <p>{item.name}</p>
-        </Link>
-      );
-    },
-  },
-  {
-    header: "24h Change",
-    cellClassName: "name-cell",
-    cell: (coin) => {
-      const item = coin.item;
-      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
-
-      return (
-        <div
-          className={cn(
-            "price-change",
-            isTrendingUp ? "text-green-500" : "text-red-500"
-          )}
-        >
-          <p>
-            {isTrendingUp ? (
-              <TrendingUp width={16} height={16} />
-            ) : (
-              <TrendingDown width={16} height={16} />
-            )}
-          </p>
-        </div>
-      );
-    },
-  },
-  {
-    header: "Price",
-    cellClassName: "price-cell",
-    cell: (coin) => coin.item.data.price,
-  },
-];
+import { Suspense } from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
 
 const Page = async () => {
-  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-    dex_pair_format: "symbol",
-  });
-
   return (
     <main className="main-container">
       <section className="home-grid">
-        <div id="coin-overview">
-          <div className="header pt-2">
-            <Image
-              src={coin.image.large}
-              alt="Bitcoin"
-              width={56}
-              height={56}
-            />
-            <div className="info">
-              <p>
-                {coin.name} / {coin.symbol.toUpperCase()}
-              </p>
-              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+        <Suspense
+          fallback={
+            <div>
+              <CoinOverviewFallback />
             </div>
-          </div>
-        </div>
+          }
+        >
+          <CoinOverview />
+        </Suspense>
 
-        <p>Trending Coins</p>
-        <DataTable
-          data={[]}
-          columns={columns}
-          rowKey={(coin) => coin.item.id}
-          tableClassName="trending-coins-table"
-        />
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
       </section>
 
       <section className="w-full mt-7 space-y-4">
--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,11 @@ export function formatCurrency(
     currency,
   }).format(value);
 }
+
+export function formatPercentage(change: number | null | undefined): string {
+  if (change === null || change === undefined || isNaN(change)) {
+    return "0.0%";
+  }
+  const formattedChange = change.toFixed(1);
+  return `${formattedChange}%`;
+}
